11d911fdf3c7d160f5d0ac67167152bf99ba91e0
ShradhaMagar12 <shradharmagar12@gmail.com>
2026-04-08 13:23:06 +0530

    Added HelloWorld and Calculator projects

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+package HelloWorld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Namaste!! You're welcomed to DevOps Lab:) Hello DevOps World!");
+    }
+}
