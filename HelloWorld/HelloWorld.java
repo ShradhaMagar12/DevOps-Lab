df5419991e52e305389e59abe2d5b607d338a294
ShradhaMagar12 <shradharmagar12@gmail.com>
2026-04-13 11:53:27 +0530

    Update HelloWorld.java

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,3 @@
-package HelloWorld;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Namaste!! You're welcomed to DevOps Lab:) Hello DevOps World!");
